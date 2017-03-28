5e7e454d6d8978d0963c644c54458bb6fd5e51f8
Geet Sethi <gsethi523@gmail.com>
2017-03-28 03:16:01 -0700

    added missing files.

---
+++ cpp/error_handling.h
@@ -0,0 +1,63 @@
+/* file: error_handling.h */
+/*******************************************************************************
+!  Copyright(C) 2014-2015 Intel Corporation. All Rights Reserved.
+!
+!  The source code, information  and  material ("Material") contained herein is
+!  owned  by Intel Corporation or its suppliers or licensors, and title to such
+!  Material remains  with Intel Corporation  or its suppliers or licensors. The
+!  Material  contains proprietary information  of  Intel or  its  suppliers and
+!  licensors. The  Material is protected by worldwide copyright laws and treaty
+!  provisions. No  part  of  the  Material  may  be  used,  copied, reproduced,
+!  modified, published, uploaded, posted, transmitted, distributed or disclosed
+!  in any way  without Intel's  prior  express written  permission. No  license
+!  under  any patent, copyright  or  other intellectual property rights  in the
+!  Material  is  granted  to  or  conferred  upon  you,  either  expressly,  by
+!  implication, inducement,  estoppel or  otherwise.  Any  license  under  such
+!  intellectual  property  rights must  be express  and  approved  by  Intel in
+!  writing.
+!
+!  *Third Party trademarks are the property of their respective owners.
+!
+!  Unless otherwise  agreed  by Intel  in writing, you may not remove  or alter
+!  this  notice or  any other notice embedded  in Materials by Intel or Intel's
+!  suppliers or licensors in any way.
+!
+!*******************************************************************************
+!  Content:
+!    Auxiliary error-handling functions used in C++ examples
+!******************************************************************************/
+
+
+#ifndef _ERROR_HANDLING_H
+#define _ERROR_HANDLING_H
+
+const int fileError = -1001;
+
+void checkAllocation(void *ptr)
+{
+    if (!ptr)
+    {
+        std::cout << "Error: Memory allocation failed" << std::endl;
+        exit(-1);
+    }
+}
+
+void fileOpenError(const char *filename)
+{
+    std::cout << "Unable to open file '" << filename << "'" << std::endl;
+    exit(fileError);
+}
+
+void fileReadError()
+{
+    std::cout << "Unable to read next line" << std::endl;
+    exit(fileError);
+}
+
+void sparceFileReadError()
+{
+    std::cout << "Incorrect format of file" << std::endl;
+    exit(fileError);
+}
+
+#endif
