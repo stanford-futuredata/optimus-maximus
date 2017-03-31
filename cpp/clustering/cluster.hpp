e07a25bef9c95810e844b93ff039fd533c98d355
Firas Abuzaid <fabuzaid21@gmail.com>
2017-03-30 20:01:40 -0700

    cosmetic changes

--- cpp/clustering/cluster.hpp
+++ cpp/clustering/cluster.hpp
@@ -3,7 +3,6 @@
 
 void random_clustering(float* input_weights, const int num_rows,
                        const int num_cols, const int num_clusters,
-                       const int num_iters, const int sample_percentage,
                        const int num_threads, float*& centroids,
                        int*& user_id_cluster_ids);
 void kmeans_clustering(float* input_weights, const int num_rows,
