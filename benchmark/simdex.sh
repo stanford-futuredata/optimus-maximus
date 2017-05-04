4c44940ff8fd0e6c56d0e3a9b23c5924dd952924
Firas Abuzaid <fabuzaid21@gmail.com>
2017-05-04 15:46:38 -0700

    small changes

--- benchmark/simdex.sh
+++ benchmark/simdex.sh
@@ -1,7 +1,14 @@
 #! /usr/bin/env bash
 
 set -x
-../cpp/simdex \
-  -w $HOME/models-simdex/lemp-paper/Netflix-noav-10 \
-  -k 1 -m 480189 -n 17770 -f 10 -c 256 -s 20 -i 1 -b 5 -t 1 \
-  --base-name lemp-paper-Netflix-noav-10
+
+if [[ $1 == "--stats" ]]; then
+  RUNNER=../cpp/simdex_stats
+else
+  RUNNER=../cpp/simdex
+fi
+
+$RUNNER \
+  -w $HOME/models-simdex/lemp-paper/Netflix-noav-50 \
+  -k 1 -m 480189 -n 17770 -f 50 -c 256 -s 10 -i 3 -b 5 -t 1 \
+  --base-name lemp-paper-Netflix-noav-50
