83fa8f5fb9aaf36f87a4f2bb01976183e9182e5b
Firas Abuzaid <fabuzaid21@gmail.com>
2017-05-22 20:14:46 -0700

    updated fexipro submodule

--- benchmark/fexipro.sh
+++ benchmark/fexipro.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
+
+if [[ $1 == "--debug" ]]; then
+  RUNNER=../fexipro/fexipro_debug
+  FLAGS="DEBUG=1"
+else
+  RUNNER=../fexipro/fexipro
+fi
+
 set -x
 
-cd ../fexipro && make clean && make && cd -
+cd ../fexipro && make clean && make $FLAGS && cd -
 
-../fexipro/fexipro \
-  -w $HOME/models-simdex/lemp-paper/Netflix-noav-10 \
-  -k 1 -m 480189 -n 17770 -f 10 \
-  --base-name lemp-paper-Netflix-noav-10
+$RUNNER \
+  -w $HOME/models-simdex/lemp-paper/Netflix-noav-50 \
+  -k 1 -m 480189 -n 17770 -f 50 \
+  --base-name lemp-paper-Netflix-noav-50
