1b999ad2ba57adc61a8face41a7a41fba8f8c82e
Firas Abuzaid <fabuzaid21@gmail.com>
2017-10-18 20:53:25 +0000

    add reg-0 for R2

--- scripts/train_models.sh
+++ scripts/train_models.sh
@@ -18,7 +18,7 @@ set -x
 ./nomad_train_netflix.sh 0.0005 50 ~/data/netflix_prize_dataset/nomad-format ~/models/nomad/Netflix-50-reg-0.0005
 
 ./nomad_train_netflix.sh 0.5 100 ~/data/netflix_prize_dataset/nomad-format ~/models/nomad/Netflix-100-reg-0.5
-./nomad_train_netflix.sh 0.05 100 ~/data/netflix_prize_dataset/nomad-format ~/models/nomad/Netflix-100-gold-standard
+./nomad_train_netflix.sh 0.05 100 ~/data/netflix_prize_dataset/nomad-format ~/models/nomad/Netflix-100-reg-0.05
 ./nomad_train_netflix.sh 0.005 100 ~/data/netflix_prize_dataset/nomad-format ~/models/nomad/Netflix-100-reg-0.005
 ./nomad_train_netflix.sh 0.0005 100 ~/data/netflix_prize_dataset/nomad-format ~/models/nomad/Netflix-100-reg-0.0005
 
@@ -64,7 +64,7 @@ set -x
 ./nomad_train.sh 0.001 50 ~/data/yahoo_KDD_cup_2011_dataset/nomad-format ~/models/nomad/KDD-50-reg-0.001
 ./nomad_train.sh 0.0001 50 ~/data/yahoo_KDD_cup_2011_dataset/nomad-format ~/models/nomad/KDD-50-reg-0.0001
 
-./nomad_train.sh 1 100 ~/data/yahoo_KDD_cup_2011_dataset/nomad-format ~/models/nomad/KDD-100-gold-standard
+./nomad_train.sh 1 100 ~/data/yahoo_KDD_cup_2011_dataset/nomad-format ~/models/nomad/KDD-100-reg-1
 ./nomad_train.sh 0.1 100 ~/data/yahoo_KDD_cup_2011_dataset/nomad-format ~/models/nomad/KDD-100-reg-0.1
 ./nomad_train.sh 0.01 100 ~/data/yahoo_KDD_cup_2011_dataset/nomad-format ~/models/nomad/KDD-100-reg-0.01
 ./nomad_train.sh 0.001 100 ~/data/yahoo_KDD_cup_2011_dataset/nomad-format ~/models/nomad/KDD-100-reg-0.001
