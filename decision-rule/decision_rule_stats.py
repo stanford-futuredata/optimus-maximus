6c7dfb7c9272200e823d78051b18156575651120
Firas Abuzaid <fabuzaid21@gmail.com>
2018-07-31 14:42:22 -0700

    Decision Rule

--- decision-rule/decision_rule_stats.py
+++ decision-rule/decision_rule_stats.py
@@ -45,19 +45,21 @@ class IndexDecisionRuleStats:
             return 480189
         elif 'KDD' in model:
             return 1000990
-        else:  # R2
+        elif 'R2' in model:
             return 1823179
+        elif 'Glove' in model:
+            return 100000
 
     def estimate_blocked_mm_runtime(self, row):
         return row[self.BLOCKED_MM_COL] * row[self.SAMPLED_USERS_COL]
 
     def blocked_mm_true_runtime(self, model, K):
-        return self.blocked_mm_truth_df.query('model == "%s" and K == %d' %
-                                              (model, K))['comp_time'].min()
+        return self.blocked_mm_truth_df.query(
+            'model == "%s" and K == %d' % (model, K))['comp_time'].min()
 
     def lemp_true_runtime(self, model, K):
-        return self.lemp_truth_df.query('model == "%s" and K == %d' %
-                                        (model, K))['comp_time'].min()
+        return self.lemp_truth_df.query(
+            'model == "%s" and K == %d' % (model, K))['comp_time'].min()
 
     def optimizer_runtime(self, row):
         overhead_rt = self.overhead_runtime(row)
@@ -200,9 +202,9 @@ class SimpleBaselineDecisionRuleStats(IndexDecisionRuleStats):
         return row[self.sample_time_col] * self.num_users(row['model'])
 
     def index_true_runtime(self, row):
-        return self.index_truth_df.query('model == "%s" and K == %d' %
-                                         (row['model'],
-                                          row['K']))['comp_time'].min()
+        return self.index_truth_df.query(
+            'model == "%s" and K == %d' % (row['model'],
+                                           row['K']))['comp_time'].min()
 
     def optimizer_runtime_no_overhead(self, row):
         return self.index_true_runtime(row) if row[
@@ -261,8 +263,8 @@ class LempSimpleBaselineDecisionRuleStats(IndexDecisionRuleStats):
         self.lemp_dec_rule_df = lemp_dec_rule_df
 
     def lemp_dec_rule_row(self, model, K):
-        single_row = self.lemp_dec_rule_df.query('model == "%s" and K == %d' %
-                                                 (model, K))
+        single_row = self.lemp_dec_rule_df.query(
+            'model == "%s" and K == %d' % (model, K))
         assert (len(single_row) == 1)
         return single_row.iloc[0]
 
@@ -305,12 +307,12 @@ class LempSimpleBaselineDecisionRuleStats(IndexDecisionRuleStats):
 
         simdex_preproc_rt = self.preproc_runtime(row)
         lemp_preproc_rt = lemp_dec_row['preproc_time'] + lemp_dec_row['index_time']
-        blocked_mm_overhead = row[self.BLOCKED_MM_COL] * row[
-            self.SAMPLED_USERS_COL]
-        simdex_overhead = row[self.sample_time_col] * row[
-            self.SAMPLED_USERS_COL]
-        lemp_overhead = lemp_dec_row[self.lemp_sample_time_col] * lemp_dec_row[
-            'num_users']
+        blocked_mm_overhead = row[self.BLOCKED_MM_COL] * row[self.
+                                                             SAMPLED_USERS_COL]
+        simdex_overhead = row[self.sample_time_col] * row[self.
+                                                          SAMPLED_USERS_COL]
+        lemp_overhead = lemp_dec_row[self.
+                                     lemp_sample_time_col] * lemp_dec_row['num_users']
 
         if self.decision_rule_winner(row) == self.LEMP:
             return simdex_preproc_rt + blocked_mm_overhead + simdex_overhead
@@ -326,9 +328,9 @@ class LempSimpleBaselineDecisionRuleStats(IndexDecisionRuleStats):
         return row[self.sample_time_col] * self.num_users(row['model'])
 
     def index_true_runtime(self, row):
-        return self.index_truth_df.query('model == "%s" and K == %d' %
-                                         (row['model'],
-                                          row['K']))['comp_time'].min()
+        return self.index_truth_df.query(
+            'model == "%s" and K == %d' % (row['model'],
+                                           row['K']))['comp_time'].min()
 
     def optimizer_runtime_no_overhead(self, row):
         winner = self.decision_rule_winner(row)
@@ -365,8 +367,7 @@ def print_stats(results_df, stats_df):
           np.sum(stats_df['index_vs_blocked_mm'] > 1.0))
     print('Index vs Lemp, Avg/Std.Dev/Min/Max:',
           np.mean(stats_df['index_vs_lemp']),
-          np.std(stats_df['index_vs_lemp']),
-          np.min(stats_df['index_vs_lemp']),
+          np.std(stats_df['index_vs_lemp']), np.min(stats_df['index_vs_lemp']),
           np.max(stats_df['index_vs_lemp']),
           np.sum(stats_df['index_vs_lemp'] > 1.0))
 
@@ -412,8 +413,12 @@ def main():
         '../plots/timing-results/kdd-blocked_mm-timing.csv')
     blocked_mm_truth_r2 = pd.read_csv(
         '../plots/timing-results/r2-blocked_mm-timing.csv')
-    blocked_mm_truth_df = pd.concat(
-        [blocked_mm_truth_netflix, blocked_mm_truth_kdd, blocked_mm_truth_r2])
+    blocked_mm_truth_glove = pd.read_csv(
+        '../plots/timing-results/glove-blocked_mm-timing.csv')
+    blocked_mm_truth_df = pd.concat([
+        blocked_mm_truth_netflix, blocked_mm_truth_kdd, blocked_mm_truth_r2,
+        blocked_mm_truth_glove
+    ])
 
     if args.index_type == RUN_LEMP:
         lemp_dec_rule_df = pd.read_csv('lemp-decision-rule.csv')
@@ -429,8 +434,12 @@ def main():
             '../plots/timing-results/kdd-simdex-timing.csv')
         simdex_truth_r2 = pd.read_csv(
             '../plots/timing-results/r2-simdex-timing.csv')
-        simdex_truth_df = pd.concat(
-            [simdex_truth_netflix, simdex_truth_kdd, simdex_truth_r2])
+        simdex_truth_glove = pd.read_csv(
+            '../plots/timing-results/glove-simdex-timing.csv')
+        simdex_truth_df = pd.concat([
+            simdex_truth_netflix, simdex_truth_kdd, simdex_truth_r2,
+            simdex_truth_glove
+        ])
         simdex_dec_rule_df = pd.read_csv('simdex-decision-rule.csv')
 
         if args.index_type == RUN_SB:
